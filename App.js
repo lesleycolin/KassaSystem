fbccd9e373d7607eac154891f805f1c7b725b33a
lesleyAllphi <160234300+lesleyAllphi@users.noreply.github.com>
2024-05-28 14:53:23 +0200

    authtentication fixed

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import * as SplashScreen from "expo-splash-screen";
 
 //hooks import
 import { useCallback, useEffect, useState } from "react";
-import { useFonts } from "expo-font";
+import * as Font from "expo-font";
 
 //redux imports
 import { Provider } from "react-redux";
@@ -18,24 +18,49 @@ import { PersistGate } from "redux-persist/integration/react";
 import RootNavigation from "./src/navigation/RootNavigation";
 import AuthContextProvider from "./src/contexts/AuthContext";
 
+//firebase imports
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./FirebaseConfig";
+
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  //state hooks
+  const [isAppReady, setIsAppReady] = useState(false);
+  const [isAuthLoading, setIsAuthLoading] = useState(true);
   const [isAuth, setIsAuth] = useState(false);
 
-  const [isFontLoaded, fontError] = useFonts({
-    "OpenSans-Bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-    OpenSans: require("./assets/fonts/OpenSans-Regular.ttf"),
-    Angkor: require("./assets/fonts/Angkor-Regular.ttf"),
-  });
+  //useEffect to check if the user is authenticated
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      // Login status te checken
+      setIsAuth(user !== null);
+      // Om te zien of we nog aan het checken zijn
+      setIsAuthLoading(false);
+
+      try {
+        await Font.loadAsync({
+          "OpenSans-Bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+          OpenSans: require("./assets/fonts/OpenSans-Regular.ttf"),
+          Angkor: require("./assets/fonts/Angkor-Regular.ttf"),
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsAppReady(true);
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   const onLayoutRootView = useCallback(async () => {
-    if (isFontLoaded || fontError) {
+    if (isAppReady) {
       await SplashScreen.hideAsync();
     }
-  }, [isFontLoaded, fontError]);
+  }, [isAppReady]);
 
-  if (!isFontLoaded && !fontError) {
+  if (!isAppReady) {
     return null;
   }
   return (

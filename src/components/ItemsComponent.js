f15adb00b1b0742590cbcec14703add25b9d5629
lesleyAllphi <160234300+lesleyAllphi@users.noreply.github.com>
2024-05-30 11:53:17 +0200

    added filter tabs for products

--- src/components/ItemsComponent.js
+++ src/components/ItemsComponent.js
@@ -1,5 +1,11 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
 import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import ItemButton from "./ItemButton";
 import { myColors } from "../utilities/Colors";
 
@@ -9,67 +15,136 @@ const ItemsComponent = () => {
       name: "Coca Cola",
       price: 2.5,
       logo: require("../../assets/logos/Coca-Cola.png"),
+      category: "Soda",
       version: "Regular",
     },
     {
       name: "Coca Cola Light",
       price: 2.5,
       logo: require("../../assets/logos/Cola-Light.png"),
+      category: "Soda",
       version: "Light",
     },
     {
       name: "Coca Cola Zero",
       price: 2.5,
       logo: require("../../assets/logos/Coca-Cola-Zero.png"),
+      category: "Soda",
       version: "Zero",
     },
     {
       name: "Fanta",
       price: 2.5,
       logo: require("../../assets/logos/Fanta.png"),
+      category: "Soda",
       version: "Regular",
     },
     {
       name: "Fanta Zero",
       price: 2.5,
       logo: require("../../assets/logos/Fanta-Zero.png"),
+      category: "Soda",
       version: "Zero",
     },
     {
       name: "Jupiler",
       price: 2.5,
       logo: require("../../assets/logos/Jupiler.png"),
-      version: "Beer",
+      category: "Beer",
+      version: "Alcohol",
     },
     {
       name: "Heineken 00%",
       price: 2.5,
       logo: require("../../assets/logos/Heineken-0.png"),
-      version: "Beer",
+      category: "Beer",
+      version: "Non-alcohol",
     },
     {
       name: "Water Plat",
       price: 2.5,
       logo: require("../../assets/logos/Spa-Blauw.png"),
+      category: "Soda",
       version: "Regular",
     },
     {
       name: "Water Bruis",
       price: 2.5,
       logo: require("../../assets/logos/Spa-Rood.png"),
+      category: "Soda",
       version: "Regular",
     },
   ]);
+  const [activeFilter, setActiveFilter] = useState("All");
+
+  // Function to filter items based on active category
+  const filteredItems = items.filter((item) => {
+    if (activeFilter === "All") {
+      return true;
+    } else {
+      return item.category === activeFilter;
+    }
+  });
 
   return (
     <View style={styles.container}>
+      {/* Filter tabs */}
+      <View style={styles.filterContainer}>
+        <TouchableOpacity
+          style={[
+            styles.filterButton,
+            activeFilter === "All" && styles.activeFilter,
+          ]}
+          onPress={() => setActiveFilter("All")}
+        >
+          <Text
+            style={[
+              styles.filterText,
+              activeFilter === "All" && styles.activeFilterText,
+            ]}
+          >
+            All
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.filterButton,
+            activeFilter === "Soda" && styles.activeFilter,
+          ]}
+          onPress={() => setActiveFilter("Soda")}
+        >
+          <Text
+            style={[
+              styles.filterText,
+              activeFilter === "Soda" && styles.activeFilterText,
+            ]}
+          >
+            Frisdrank
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.filterButton,
+            activeFilter === "Beer" && styles.activeFilter,
+          ]}
+          onPress={() => setActiveFilter("Beer")}
+        >
+          <Text
+            style={[
+              styles.filterText,
+              activeFilter === "Beer" && styles.activeFilterText,
+            ]}
+          >
+            Bier
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* Item list */}
       <FlatList
-        justifyContent="center"
-        margin="auto"
+        contentContainerStyle={styles.flatListContainer}
         numColumns={3}
         keyExtractor={(item) => item.name.toString()}
-        style={styles.itemsList}
-        data={items}
+        data={filteredItems}
         renderItem={({ item }) => (
           <ItemButton
             name={item.name}
@@ -83,14 +158,35 @@ const ItemsComponent = () => {
   );
 };
 
-export default ItemsComponent;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: myColors.backGround,
   },
-  itemsList: {
-    display: "flex",
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    paddingVertical: 10,
+  },
+  filterButton: {
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: myColors.primary,
+    width: 80,
+  },
+  activeFilter: {
+    backgroundColor: myColors.primary,
+  },
+  filterText: {
+    color: myColors.primary,
+  },
+  activeFilterText: {
+    color: myColors.white,
+  },
+  flatListContainer: {
+    alignItems: "center",
   },
 });
+
+export default ItemsComponent;

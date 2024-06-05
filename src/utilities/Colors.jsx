351af03421f023fef8a315e57272be217db63370
Lesley Colin <djlesley78@gmail.com>
2024-06-05 10:46:47 +0200

    background color back to original

--- src/utilities/Colors.jsx
+++ src/utilities/Colors.jsx
@@ -2,7 +2,7 @@ export const myColors = {
   primary: "#EEEEEE",
   secondary: "#CADEFC",
   third: "#c8c8c8",
-  backGround: "#aaaaaa",
+  backGround: "#222831",
   focus: "#00ADB5",
   input: "#555",
 };
